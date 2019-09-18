fa9cf6d9fea66c52e10a916a50b1af9f12bc3020
Danzel Rana <danzelrana@gmail.com>
2019-09-18 16:42:27 -0700

    Updates

---
+++ precache-manifest.7887f882ba586f828c8edd592bd2c93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99071233174e217eb8769817ffc3bbd",
+    "url": "/marvel-encyclopedia/index.html"
+  },
+  {
+    "revision": "36d77502f1975ae21f0f",
+    "url": "/marvel-encyclopedia/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b64a840e4e21dfc82474",
+    "url": "/marvel-encyclopedia/static/css/main.9322591f.chunk.css"
+  },
+  {
+    "revision": "36d77502f1975ae21f0f",
+    "url": "/marvel-encyclopedia/static/js/2.569d8a35.chunk.js"
+  },
+  {
+    "revision": "b64a840e4e21dfc82474",
+    "url": "/marvel-encyclopedia/static/js/main.4d594603.chunk.js"
+  },
+  {
+    "revision": "fd4a00f8277a3c1b36af",
+    "url": "/marvel-encyclopedia/static/js/runtime~main.98c20808.js"
+  }
+]);
